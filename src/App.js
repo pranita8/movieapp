fad1961d152f4f8a6a43d5dc0e65161ac7ebc563
pranita8 <pranitamore842@gmail.com>
2023-03-27 16:44:57 +0530

    initial changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './Header';
+import Movie from './Movie';
+import movies from "./movie.json";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        {
+          movies.map((element,index) => {
+            return (
+              <Movie
+                key={index}
+                title={element.Title}
+                year={element.Year}
+                img={element.Poster}
+              />
+            );
+            
+          }
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+const Header=()=>{
+    return(
+        <div className="head">
+       <h1>Movies</h1> 
+       </div>
+    )
+}
+export default Header;
